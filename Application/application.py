85ab29e1611e8ab6f4439a3585ab9d443018dd2a
Laura Xavier <laura.xavier@dtidigital.com.br>
2022-12-02 12:26:14 -0300

    find max disjoint paths

--- Application/application.py
+++ Application/application.py
@@ -5,24 +5,25 @@ sys.path.insert(0, '../Model')
 
 # Importando funções e objetos de outros arquivos
 from graph import Graph
+from copy import deepcopy
 
 # Ler o arquivo que tem todas as arestas
 def create_graph(arq, graph):
     for linha in arq:
         u, v = linha.split(' ')
-        graph.addEdges(int(u), int(v))
+        graph.add_edge(int(u), int(v))
 
 def main():
     dir_name = sys.argv[1]
-    vertice_origem = sys.argv[2]
-    vertice_destino = sys.argv[3]
+    origin = sys.argv[2]
+    destiny = sys.argv[3]
 
     arq = open(dir_name, 'r')
-    num_vertices = int(arq.readline())
 
-    graph = Graph(num_vertices, vertice_origem, vertice_destino)
-    create_graph(arq, graph)
-    caminhos, maximo = graph.get_disjoint_paths(vertice_origem, vertice_destino)
+    graph = Graph(arq)
+
+    print ("There can be maximum %d edge-disjoint paths from %d to %d" %
+			(graph.get_disjoint_paths(int(origin), int(destiny)), int(origin), int(destiny)))
 
 
 # Programa principal
--- Model/graph.py
+++ Model/graph.py
@@ -1,26 +1,102 @@
+from copy import deepcopy
+
+
 class Graph:
-    def __init__(self, num_vertices, origem, destino):
-        self.num_vertices = num_vertices
-        self.graph = list()
-        self.verticeOrigem = origem
-        self.verticeDestino = destino
-    
-    def addEdges(self, u, v):
-        self.graph.append([u, v])
+    def __init__(self, arq):
+        self.read_graph(arq)
 
-    def create_adjacency_list(self):
-        listaAdjacencia = [[] for i in range(self.num_vertices)] # Inicialização do array
-        for i in range(len(self.graph)):
-            listaAdjacencia[self.graph[i][0]].append(self.graph[i][1])
-        return listaAdjacencia
+    def read_graph(self, arq):
+        self.num_nodes = int(arq.readline())
+        self.adj_list = [[] for i in range(self.num_nodes)]
+        for linha in arq:
+            u, v = linha.split(' ')
+            self.add_edge(int(u), int(v))
 
-    def get_disjoint_paths(self, origem, destino):
-        paths = list() # Lista com os caminhos percorridos
-        max_paths = 0 # Quantidade máxima de caminhos
-        adjacencyList = self.create_adjacency_list()
+    def add_edge(self, u, v):
+        self.adj_list[u].append(v)
 
-        return paths, max_paths
-    
-    def printGraph(self):
-        for i in range(0, len(self.graph)):
-            print(self.graph[i])
+    def remove_edge(self, u, v):
+        self.adj_list[u].remove(v)
+
+        '''Returns true if there is a path from source 's' to sink 't' in
+	residual graph. Also fills parent[] to store the path '''
+
+    def search(self, s, t, parent):
+
+        # Mark all the vertices as not visited
+        visited = [False]*(self.num_nodes)
+
+        # Create a queue for BFS
+        queue = []
+
+        # Mark the source node as visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        # Standard BFS Loop
+        while queue:
+
+            # Dequeue a vertex from queue and print it
+            u = queue.pop(0)
+
+            # Get all adjacent vertices of the dequeued vertex u
+            # If a adjacent has not been visited, then mark it
+            # visited and enqueue it
+            for val in self.adj_list[u]:  # adj[u] = {2, 3, 4}
+                if visited[val] == False:
+                    queue.append(val)
+                    visited[val] = True
+                    parent[val] = u
+
+        # If we reached sink in BFS starting from source, then return
+        # true, else false
+        return True if visited[t] else False
+
+        # Returns the maximum number of edge-disjoint paths from
+        # s to t in the given graph
+
+    def is_edge(self, u, v):
+        if v in self.adj_list[u]:
+            return 1
+        return 0
+
+    def get_disjoint_paths(self, source, sink):
+        aux_graph = deepcopy(self)
+
+        # This array is filled by BFS and to store path
+        parent = [-1]*(aux_graph.num_nodes)
+        max_flow = 0  # There is no flow initially
+
+        # Augment the flow while there is path from source to sink
+        while aux_graph.search(source, sink, parent):
+
+            # Find minimum residual capacity of the edges along the
+            # path filled by BFS. Or we can say find the maximum flow
+            # through the path found.
+            path_flow = float("Inf")
+            s = sink
+            while (s != source):
+                path_flow = min(path_flow, aux_graph.is_edge(parent[s], s))
+                s = parent[s]
+
+            # Add path flow to overall flow
+            max_flow += path_flow
+
+            # update residual capacities of the edges and reverse edges
+            # along the path
+            v = sink
+            while (v != source):  # 1 2 3 5
+                u = parent[v]
+                aux_graph.remove_edge(u, v)
+                aux_graph.add_edge(v, u)
+                v = parent[v]
+
+        return max_flow
+
+    def print(self):
+        for i in range(self.num_nodes):
+            for j in self.adj_list[i]:
+                print(i, j)
+
+    def print_adj_list(self):
+        print(self.adj_list)
