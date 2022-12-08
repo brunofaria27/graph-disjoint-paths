afb4b125563ee5e53365a0d6a5e7b642cf52d7bd
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2022-12-08 11:45:00 -0300

    debug

--- Application/debug.py
+++ Application/debug.py
@@ -1,11 +1,85 @@
 import sys
 import random
 
-# Importando pastas a sere utilizadas
-sys.path.insert(0, '../Model')
+from copy import deepcopy
 
-# Importando funções e objetos de outros arquivos
-from graph import Graph
+class Graph:
+    def __init__(self, arq):
+        self.read_graph(arq)
+
+    def read_graph(self, arq):
+        self.num_nodes = int(arq.readline())
+        self.adj_list = [[] for i in range(self.num_nodes)]
+        for linha in arq:
+            u, v = linha.split(' ')
+            self.add_edge(int(u), int(v))
+
+    def add_edge(self, u, v):
+        self.adj_list[u].append(v)
+
+    def remove_edge(self, u, v):
+        self.adj_list[u].remove(v)
+
+    def is_edge(self, u, v):
+        if v in self.adj_list[u]:
+            return 1
+        return 0
+
+    def search(self, s, t, parent):
+        visited = [False] * (self.num_nodes)
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            u = queue.pop(0)
+            for val in self.adj_list[u]:  # adj[u] = {2, 3, 4}
+                if visited[val] == False:
+                    queue.append(val)
+                    visited[val] = True
+                    parent[val] = u
+        if visited[t]:
+            return True
+        return False
+
+    def get_disjoint_paths(self, source, sink):
+        aux_graph = deepcopy(self)
+        parent = [-1] * (aux_graph.num_nodes)
+        max_flow = 0
+        paths = []
+
+        while aux_graph.search(source, sink, parent):
+            path_flow = float("Inf")
+            path = []
+            s = sink
+
+            while (s != source):
+                path_flow = min(path_flow, aux_graph.is_edge(parent[s], s))
+                path.append(s)
+                s = parent[s]
+        
+            path.append(source)
+            path.reverse()
+            paths.append(path)
+            max_flow += path_flow
+
+            v = sink
+            while (v != source):
+                u = parent[v]
+                aux_graph.remove_edge(u, v)
+                aux_graph.add_edge(v, u)
+                v = parent[v]
+        return max_flow, paths
+
+    def print(self):
+        for i in range(self.num_nodes):
+            for j in self.adj_list[i]:
+                print(i, j)
+
+    def print_adj_list(self):
+        print(self.adj_list)
+
 
 def looping_aux(start, end):
     edges = list()
@@ -65,9 +139,9 @@ def create_graph(arq, graph):
         graph.add_edge(int(u), int(v))
 
 def main():
-    dir_name = sys.argv[1]
-    origin = sys.argv[2]
-    destiny = sys.argv[3]
+    dir_name = input()
+    origin = input()
+    destiny = input()
 
     create_complete_graph(dir_name, 10)
     arq = open(dir_name, 'r')
